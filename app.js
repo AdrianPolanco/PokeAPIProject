2417eb59c5199f152220da96c8fc1dcadfb43e1d
Adrian Saul Polanco Ferrer <116674818+AdrianPolanco@users.noreply.github.com>
2023-02-27 18:41:24 -0400

    Parte principal maquetada

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import { startApp } from "./scripts/open.js";
 
+/*Relevant links:
+
+https://pokeapi.co/api/v2/pokemon-species/7/
+
+https://pokeapi.co/api/v2/evolution-chain/3/
+*/
+
 const startButton = document.querySelector("#startButton");
 
 document.addEventListener("DOMContentLoaded", function (e) {
