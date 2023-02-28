94e4f934b4316164f44310000ca06d3a2276f39e
Adrian Saul Polanco Ferrer <116674818+AdrianPolanco@users.noreply.github.com>
2023-02-28 12:54:45 -0400

    Divs mostrados en el scroll

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import { startApp } from "./scripts/open.js";
+import { cargarScroll } from "./scripts/scroll.js";
 
 /*Relevant links:
 
 https://pokeapi.co/api/v2/pokemon-species/7/
 
 https://pokeapi.co/api/v2/evolution-chain/3/
+
+https://pokeapi.co/api/v2/type/
 */
 
 const startButton = document.querySelector("#startButton");
 
 document.addEventListener("DOMContentLoaded", function (e) {
+    cargarScroll();
     startButton.addEventListener("click", function (e) {
         startApp();
     });
