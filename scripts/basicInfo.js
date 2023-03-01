308114bd31769986c656e8cce0bc72a821235699
Adrian Saul Polanco Ferrer <116674818+AdrianPolanco@users.noreply.github.com>
2023-03-01 11:00:53 -0400

    Imagenes y habilidades añadidas/Images and abilities added

--- scripts/basicInfo.js
+++ scripts/basicInfo.js
@@ -1,5 +1,7 @@
 const nameInfo = document.querySelector("#name");
 const numberInfo = document.querySelector("#pokedex-number");
+const mainImage = document.querySelector("#mainImage");
+const abilitiesData = document.querySelector("#abilities");
 export const descriptionText = document.querySelector("#description-text");
 
 export let versionsObj = {
@@ -9,6 +11,12 @@ export let versionsObj = {
 
 export function basicInfo(name, number) {
     const urlSpecies = `https://pokeapi.co/api/v2/pokemon-species/${name.toLowerCase()}/`;
+    const urlDetails = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+
+    fetch(urlDetails)
+        .then((response) => response.json())
+        .then((data) => showDetails(data));
+
     fetch(urlSpecies)
         .then((answer) => answer.json())
         .then((data) =>
@@ -31,4 +39,29 @@ function successBasicInfo(name, number, description = []) {
 
     versionsObj.sword = description[identifiedInfoSword].flavor_text;
     versionsObj.shield = description[identifiedInfoShield].flavor_text;
+}
+
+/*  foto.src = obj.sprites.other["official-artwork"].front_default
+
+  height.textContent = `Altura: ${obj.height}
+  
+  https://pokeapi.co/api/v2/pokemon/squirtle*/
+
+export function showDetails(obj) {
+    mainImage.src = obj.sprites.other["official-artwork"]["front_default"];
+
+    obj["abilities"].forEach((ability) => {
+        const abilityData = document.createElement("p");
+        abilityData.classList.add("fs-4", "fw-normal");
+        abilityData.textContent = `${ability[
+            "ability"
+        ].name[0].toUpperCase()}${ability["ability"].name.slice(1)}`;
+        abilitiesData.appendChild(abilityData);
+
+        /*<p class="fs-4 fw-normal">Pressure</p>*/
+
+        /*let string = "Hola";
+        console.log(`${string[0]}${string.slice(1)}`);*/
+    });
+    //abilitiesData.textContent =
 }
